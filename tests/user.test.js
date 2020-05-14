733731b5c29ebf29c07a75356d436c055cc60efd
KelvinMitaki <mitakikelvin1@gmail.com>
2020-05-14 16:05:55 +0300

    added test cases for getting users when authenticated

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,12 +1,22 @@
 const request = require("supertest");
 const app = require("../src/app");
 const User = require("../src/models/user");
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+
+const userId = new mongoose.Types.ObjectId();
 
 const testUser = {
+  _id: userId,
   name: "test",
   email: "test@gmail.com",
   password: "test@gmail.com",
   age: 25,
+  tokens: [
+    {
+      token: jwt.sign({ _id: userId }, process.env.JWT_SECRET),
+    },
+  ],
 };
 
 beforeEach(async () => {
@@ -44,4 +54,16 @@ test("should not login non existent user", async () => {
       password: "jkshkjhdsjkhdkhsdkj",
     })
     .expect(404);
+});
+
+test("should get profile for authenticated user", async () => {
+  await request(app)
+    .get("/users/me")
+    .set("Authorization", `Bearer ${testUser.tokens[0].token}`)
+    .send()
+    .expect(200);
+});
+
+test("should not get profile for unauthenticated users", async () => {
+  await request(app).get("/users/me").send().expect(401);
 });
